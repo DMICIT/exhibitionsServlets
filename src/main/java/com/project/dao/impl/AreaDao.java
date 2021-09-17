a3ecce23640184dffb14da16746b5eb2f4c75d47
Dmytro Mikhaltsov <mikhaltsovda@gmail.com>
2021-09-17 15:26:22 +0300

    03 Add CRUD operations

--- src/main/java/com/project/dao/impl/AreaDao.java
+++ src/main/java/com/project/dao/impl/AreaDao.java
@@ -1,11 +1,8 @@
 package com.project.dao.impl;
-
 import com.project.dao.EntityDao;
 import com.project.entities.Area;
 import com.project.persistance.DataSourceFactory;
 import org.apache.log4j.Logger;
-
-import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,9 +16,8 @@ public class AreaDao implements EntityDao<Area> {
     @Override
     public List<Area> getAll() {
         List<Area> result = new ArrayList<>();
-        try {
-            Connection connection = DataSourceFactory.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM areas");
+        try (Connection connection = DataSourceFactory.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM areas");){
             ResultSet resultSet = preparedStatement.getResultSet();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -29,7 +25,7 @@ public class AreaDao implements EntityDao<Area> {
                 Area areaData = new Area(id, area);
                 result.add(areaData);
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             LOG.error(e.getMessage(), e);
         }
         return result;
@@ -39,18 +35,46 @@ public class AreaDao implements EntityDao<Area> {
     public Area getById(int inputId) {
         Area areaData = null;
 
-        try {
-            Connection connection = DataSourceFactory.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM areas WHERE id = ?");
-            preparedStatement.setInt(1,inputId);
+        try (Connection connection = DataSourceFactory.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM areas WHERE id = ?");){
+            preparedStatement.setInt(1, inputId);
             ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()){
+            if (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 int area = resultSet.getInt("area");
-                areaData = new Area(id,area);
+                areaData = new Area(id, area);
             }
         } catch (SQLException e) {
-            LOG.error(e.getMessage(),e);
-        } return areaData;
+            LOG.error(e.getMessage(), e);
+        }
+        return areaData;
+    }
+
+    @Override
+    public int create(Area entity) {
+
+        int result = 0;
+        try (Connection connection = DataSourceFactory.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO areas(area) VALUES (?)");){
+            preparedStatement.setInt(1, entity.getArea());
+            result = preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return result;
+    }
+
+    @Override
+    public int update(Area entity) {
+        int result = 0;
+        try (Connection connection = DataSourceFactory.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("UPDATE areas SET(area) VALUES (?)");) {
+            preparedStatement.setInt(1, entity.getArea());
+            result = preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
     }
 }
